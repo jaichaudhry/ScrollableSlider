55d8da3d8f3d6beead59f56d2334bb173b101e19
jaycee <jaichaudhry@users.noreply.github.com>
2017-08-20 15:49:35 +0530

    Adding scrollabe slider project.

---
+++ scrollableSlider/scrollableSlider/scrollableSlider.h
@@ -0,0 +1,19 @@
+//
+//  scrollableSlider.h
+//  scrollableSlider
+//
+//  Created by Jai Chaudhry on 20/08/17.
+//  Copyright © 2017 jc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for scrollableSlider.
+FOUNDATION_EXPORT double scrollableSliderVersionNumber;
+
+//! Project version string for scrollableSlider.
+FOUNDATION_EXPORT const unsigned char scrollableSliderVersionString[];
+
+#import <scrollableSlider/JCScrollableSlider.h>
+
+
